0a52c748ce5793c1b2e43360a788c0f1c9498d27
priyankacs123 <104245585+priyankacs123@users.noreply.github.com>
2023-04-10 23:08:29 +0530

    Add files via upload

---
+++ 05-04-2023/ecommerce/src/app/det/det.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-det',
+  templateUrl: './det.component.html',
+  styleUrls: ['./det.component.css']
+})
+export class DetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
